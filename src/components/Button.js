ab30517347e0ae5c2ae52a3c89c5218d24039f60
Zak <zak.johansson@gmail.com>
2023-01-10 10:51:14 -0800

    add buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import "components/Button.scss";
+import 'components/Button.scss';
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = 'button';
+
+  if (props.confirm) {
+    buttonClass += ' button--confirm';
+  }
+
+  return <button>{props.children}</button>;
 }
