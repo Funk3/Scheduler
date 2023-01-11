3f0577f025a35fd169fc2456e4b35234a7e13647
Zak <zak.johansson@gmail.com>
2023-01-11 11:08:36 -0800

    change values of Daylist and affected files

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import InterviewerListItem from './InterviewerListItem';
 import 'components/InterviewerList.scss';
-import PropTypes from 'prop-types';
 
 export default function interviewerList(props) {
   const interviewers = props.interviewers.map((interviewer) => {
@@ -23,7 +22,3 @@ export default function interviewerList(props) {
     </section>
   );
 }
-
-interviewerList.propTypes = {
-  interviewers: PropTypes.array.isRequired,
-};
